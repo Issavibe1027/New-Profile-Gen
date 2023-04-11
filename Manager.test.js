efdc56318fdad70573dec84a3d9dc3a5ef5adfe8
Issavibe1027 <115046426+Issavibe1027@users.noreply.github.com>
2023-04-11 10:37:29 -0400

    Update Manager.test.js

--- Manager.test.js
+++ Manager.test.js
@@ -1,16 +1,13 @@
-// using Manager constructor 
 const Manage = require('../lib/Manage');
 
-// creating manager object  
-test('creates an Manager object', () => {
+test('creates an Manage object', () => {
     const manage = new Manage('Issa', 90, 'Issa.chamblee@gmail', 4);
     
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
-// gets role from getRole()
 test('gets role of employ', () => {
     const manage = new Manage('Issa', 90, 'issa.chamblee@gmail.com');
 
-    expect(manager.getRole()).toEqual("Manage");
+    expect(manage.getRole()).toEqual("Manage");
 }); 
